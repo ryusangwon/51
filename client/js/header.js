b8bb2c7ab72cda7554f8ae5904c3048e682f0233
moniko1 <112869772+moniko1@users.noreply.github.com>
2022-10-27 19:25:18 +0900

    Update header.js

--- client/js/header.js
+++ client/js/header.js
@@ -1,11 +1,11 @@
 function logo_click() {
-  location.href="file:///C:/Users/Dahun/Desktop/%EC%BA%A1%EC%8A%A4%ED%86%A4%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/%EC%BA%A1%EC%8A%A4%ED%86%A4%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/header.html";
+  location.href="header.html";
 }
 
 function menu1_click() {
-  location.href="file:///C:/Users/Dahun/Desktop/%EC%BA%A1%EC%8A%A4%ED%86%A4%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/%EC%BA%A1%EC%8A%A4%ED%86%A4%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/lecture_find.html";
+  location.href="lecture_find.html";
 }
 
 function menu2_click() {
-  location.href="file:///C:/Users/Dahun/Desktop/%EC%BA%A1%EC%8A%A4%ED%86%A4%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/%EC%BA%A1%EC%8A%A4%ED%86%A4%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/rcm_board.html";
+  location.href="rcm_board.html";
 }
