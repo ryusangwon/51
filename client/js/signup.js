e54f0cf9189c82983451e98dcdd141db0c22da5b
moniko1 <112869772+moniko1@users.noreply.github.com>
2022-10-27 19:16:37 +0900

    Add files via upload

---
+++ client/js/signup.js
@@ -0,0 +1,7 @@
+function signup() {
+  var id = document.getElementById("id").value;
+  var pwd = document.getElementById("password").value;
+  var name = document.getElementById("name").value;
+  
+  alert("아이디:" + id + ", 비밀번호: " + pwd+ ", 이름: " + name);
+}
