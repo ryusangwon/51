88ee83f2d388a9cb422e20fcece45bc8223219eb
ryusangwon <r8uuuuuuuu@gmail.com>
2022-10-31 19:39:45 +0900

    connect database

--- server/app.js
+++ server/app.js
@@ -1,28 +1,46 @@
-var express = require('express');
+const express = require('express');
 const http = require('http');
 const path = require('path');
-// const mysql = require('mysql')
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '0000',
-//     database: '51-mysql'
-// })
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 
-// connection.connect();
-// connection.query('SELECT * from ')
-
-var app = express();
+const { sequelize } = require('./models');
+const app = express();
 const port = 3000;
-// let hostname = "localhost";
-
-// app.use(express.json());
+let hostname = "localhost";
 
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true
+});
+
+sequelize.sync({force: false}).then(() => {
+    console.log('Database connected successfully');
+}).catch((err) => {
+    console.error(err);
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} no router.`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err: {};
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/header.html'))
-})
+});
 
 app.get("/user/:id", (req, res) => {
 	res.send(`User id is ${req.params.id}`); // Or res.send('User id is ' + req.params.id);
