2fd93de52226fa997c0e032bf5013f37c6c64ced
moniko1 <112869772+moniko1@users.noreply.github.com>
2022-11-07 03:08:15 +0900

    Add files via upload

--- client/js/header.js
+++ client/js/header.js
@@ -2,14 +2,22 @@ function logo_click() {
   location.href="header.html";
 }
 
-function menu1_click() {
+function lecture_click() {
   location.href="lecture_find.html";
 }
 
-function menu2_click() {
+function rmc_click() {
   location.href="rcm_board.html";
 }
 
-function menu3_click() {
-  location.href="mypage.html";
+function mypage_click() {
+  location.href="mypage_course_list.html";
+}
+
+function login_click() {
+  location.href="login.html";
+}
+
+function signup_click() {
+  location.href="signup.html";
 }
