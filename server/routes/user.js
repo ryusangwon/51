ea928c16a7fd7d60cd34b609ada88b76684ed788
ryusangwon <r8uuuuuuuu@gmail.com>
2022-11-24 19:15:24 +0900

    Add get lecture

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,7 +2,18 @@ const express = require("express");
 const passport = require('passport');
 const User = require("../models/user");
 const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
+const Sequelize = require("sequelize");
+const {QueryTypes} = require("sequelize");
 const router = express.Router();
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config.json')[env];
+const sequelize = new Sequelize(
+        config.database,
+        config.username,
+        config.password,
+        config
+        );
 
 router.get('/', async (req, res, next) => {
     try{
@@ -63,12 +74,35 @@ router.get('/logout', isLoggedIn, (req, res) => {
 })
 
 
-router.post('/ismenti', async (req, res) => {
+router.post('/ismento', async (req, res) => {
     let id = req.body.id;
     const exUser = await User.findOne({where: {gosok_id: id}});
     if (exUser) {
         if (exUser.mento === true){
             return res.send("mento");
+        } else{
+            return res.send("no mento");
+        }
+    } else{
+        return res.send("No User");
+    }
+})
+
+router.post('/getLecture', async (req, res) => {
+    let id = req.body.id;
+//    let id = 1;
+    let query;
+    const exUser = await User.findOne({where: {id: id}});
+    console.log(exUser);
+    if (exUser) {
+        if (exUser.mento === true){
+            query = 'SELECT * FROM lecture_user, lecture WHERE lecture_user.mento_id = (SELECT user.game_id FROM user where user.id=1) AND lecture.id = lecture_user.lecture_id';
+            const result = await sequelize.query(query, {
+                type: QueryTypes.SELECT,
+                replacements: [id],
+            });
+            console.log(result);
+            return res.send(result);
         } else{
             return res.send("no mento");
         }
