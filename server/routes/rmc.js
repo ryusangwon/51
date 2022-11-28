cb664ab017f8e744c46a20546a78212671067339
Dahun Mo <moniko1@naver.com>
2022-11-28 20:35:11 +0900

    -

--- server/routes/rmc.js
+++ server/routes/rmc.js
@@ -4,6 +4,16 @@ const Rmc = require('../models/rmc');
 
 const router = express.Router();
 
+router.get('/', async (req, res, next) => {
+  try{
+      console.log("[RMC]");
+      res.send('Hello, Rmc');
+  } catch(err){
+      console.error(err);
+      next(err);
+  }
+});
+
 router.post('/create', async (req, res, next) => {
     const {id} = req.params.body.id;
     const {user_id} = req.params.body.user_id;
@@ -33,6 +43,17 @@ router.put('/update', async (req, res, next) => {
     }
   );
   res.send('update done');
+});
+
+router.post('/rmclist', async (req, res) => {
+  //작성한 정보 가져옴
+  const { borderDate, borderUserNick, borderPwd, borderTitle, borderContent } = req.body;
+  //유효성 검사
+  isExist = await borders.find({ borderDate });
+  if (isExist.length == 0) {
+    await borders.create({ borderDate, borderUserNick, borderPwd, borderTitle, borderContent });
+  }
+  res.send({ result: "success" });
 });
 
 module.exports = router;
