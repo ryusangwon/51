983cb65d80b14124f97523aeafac3f3b8b9a9dd3
Dahun Mo <moniko1@naver.com>
2022-12-11 14:35:05 +0900

    Main page update

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import Header from './Header';
+import { Link } from 'react-router-dom'
 import './css/main.css';
 
 import search_img from './img/main_search_img.png';
 import rmc_img from './img/main_rmc_img.jpg';
+import mypage_img from './img/main_mypage_img.png';
 
 const Main = () => {
   return (
     <div>
       <Header/>
 
+      <Link to="./lecture">
+      <img className="main_lecture_div" src={search_img} />
+      </Link>
 
-      <img className="main_lecture_div" src={search_img}>
+      <Link to="./Rmc">
+      <img className="main_rmc_div" src={rmc_img} />
+      </Link>
 
-      </img>
-      <img className="main_rmc_div" src={rmc_img}>
-
-      </img>
-      <img className="main_mypage_div">
-
-      </img>
+      <Link to="./Mypage_course_list">
+      <img className="main_mypage_div" src={mypage_img} />
+      </Link>
     </div>
   );
 };
